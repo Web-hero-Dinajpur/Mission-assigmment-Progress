aaf044e66f4e9b474f073e691f5dc6052121d0cb
sreedhar <sreedharroy0@gmailcom>
2026-03-03 09:57:40 -0600

    first commit Navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './Components/Shared/Navbar.jsx/Navbar'
+function App() {
+
+
+  return (
+    <>
+      <div className='w-[1280px] mx-auto'>
+        <Navbar></Navbar>
+      </div>
+    </>
+  )
+}
+
+export default App
