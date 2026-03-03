c10c84d1111e5d8b781a7a9ac5b8da4b3b88dc04
sreedhar <sreedharroy0@gmailcom>
2026-03-03 11:46:34 -0600

    update footer section

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -1,46 +1,52 @@
 import React from 'react';
+import { FaFacebook, FaLinkedin } from 'react-icons/fa';
+import { GrWebcam } from 'react-icons/gr';
+import { RiXboxLine } from 'react-icons/ri';
 
 const Footer = () => {
     return (
-        <footer className="footer sm:footer-horizontal bg-black text-white p-10 grid grid-cols-4">
-            <aside>
-                <h2>CS — Ticket System
-                </h2>
-                <p className='md:w-[250px]'>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy
-                    text ever since the 1500s, when an
-                    unknown printer took a galley of type and
-                    scrambled it to make a type specimen book.
-                </p>
-            </aside>
-            <nav>
-                <h6 className="footer-title">Services</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Company</h6>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Legal</h6>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Legal</h6>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
-            </nav>
-        </footer>
+        <>
+            <footer className="footer sm:footer-horizontal bg-black text-white p-10 grid grid-cols-4">
+                <aside>
+                    <a className="btn-ghost text-2xl font-bold">CS — Ticket System</a>
+                    <p className='md:w-[250px] text-justify'>
+                        Smart, secure, and reliable ticket management system designed for modern support teams. Easily create, track, and resolve customer issues with efficiency. Improve communication, boost productivity, and deliver exceptional service experiences through an organized and user-friendly platform built for success.
+                    </p>
+                </aside>
+                <nav>
+                    <h6 className="footer-title">Company</h6>
+                    <a className="link link-hover">About Us</a>
+                    <a className="link link-hover">Our Mission</a>
+                    <a className="link link-hover">Contact Saled</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Services</h6>
+                    <a className="link link-hover">Products & Services</a>
+                    <a className="link link-hover">Customer Stories</a>
+                    <a className="link link-hover">Download Apps</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Information</h6>
+                    <a className="link link-hover">Privacy Policy</a>
+                    <a className="link link-hover">Terms & Conditions</a>
+                    <a className="link link-hover">Join Us</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Social Links</h6>
+                    <a className="link link-hover flex gap-2 items-center"><RiXboxLine />@CS — Ticket System</a>
+                    <a className="link link-hover flex gap-2 items-center"><FaFacebook />P@CS — Ticket System</a>
+                    <a className="link link-hover flex gap-2 items-center"><FaLinkedin />@CS — Ticket System</a>
+                    <a className="link link-hover flex gap-2 items-center"><GrWebcam />support@cst.com</a>
+                </nav>
+            </footer>
+            <hr className='' />
+            <footer className="footer sm:footer-horizontal footer-center bg-black text-white p-4">
+                <aside>
+                    <p>Copyright © {new Date().getFullYear()}2026 CS — Ticket System. All rights reserved.</p>
+                </aside>
+            </footer>
+        </>
+
     );
 };
 
